1f3e427fe79c760d69de1e010a3fd1558ea91629
karel-rehor <karel.k.rehor@email.cz>
2019-03-08 14:18:53 +0100

    refactor of core app

--- app.js
+++ app.js
@@ -21,76 +21,117 @@ app.get('/login', function(req, res){
 })
 
 
-app.post('/login', function(req, res){
+function authenticUser(req, res, cbSuccess, cbFail){
 
-    //console.log(req.body())
     var bodyStr = '';
     var username;
-    var password;
-    var params;
+    var credentials;
 
     req.on("data", function(chunk){
         bodyStr += chunk.toString();
     })
 
     req.on('end', function(){
-        console.log("bodyStr: #" + bodyStr + "#")
 
-        params = bodyStr.split('&');
-        params.forEach((param) => {
-            console.log(param.split('=')[0])
-            switch(param.split('=')[0]){
-                case 'username':
-                    username = param.split('=')[1];
-                    break;
-                case 'password':
-                    password = param.split('=')[1]
-                    break;
-                default:
-                    console.log("unknown param " + param);
-                    break;
-            }
+        credentials = getCredentials(bodyStr);
 
+        while(!credentials){} //wait until credentials are filled which happens after function call above
 
-        })
-
-
-        while(!params){} //wait until params are filled which happes after request.on above
-
-        console.log("username: " + username)
-        console.log("password: " + password)
-
+        username = Object.keys(credentials)[0]
 
         res.cookie('username', username)
 
-        if(passwords[username] == password){
-        //if(username == 'krtek' && password == 'noname') {
-            console.log("Got credentials: " + username + ":" + passwords[username])
-            res.cookie('id_token', 'OK', {expires: new Date(Date.now() + 9000000)})
-            res.redirect('/treasure')
-            //res.sendFile("./poklad.html")
+
+        if(passwords[username] === credentials[username]){
+            cbSuccess();
         }else{
-            res.redirect('/')
+            cbFail();
+        }
+
+    })
+
+}
+
+function getCredentials(str){
+
+    var response = {};
+    var username;
+    var password;
+
+    params = str.split('&');
+    params.forEach((param) => {
+        switch(param.split('=')[0]){
+            case 'username':
+                username = param.split('=')[1];
+                break;
+            case 'password':
+                password = param.split('=')[1]
+                break;
+            default:
+                break;
         }
 
 
     })
 
+    if( username !== undefined && password != undefined){
+        response[username] = password;
+    }
+
+    return response;
+
+}
+
+function hasIdToken(req, res, cbSuccess, cbFail){
+
+    if(req.get('cookie').indexOf('id_token=OK') !== -1) {
+        cbSuccess();
+    }else{
+        cbFail();
+    }
+}
+
+app.post('/login', function(req, res){
+
+    authenticUser(req, res, function(){
 
 
+        console.log("DEBUG Calling Success callback")
+
+        res.cookie('id_token', 'OK', {expires: new Date(Date.now() + 9000000)})
+        res.redirect('/treasure')
+    },
+        function(){
+
+            console.log("DEBUG Calling Fail callback")
+
+        res.redirect('/')
+
+    });
 
 })
 
 app.get('/treasure', function(req, res){
-    console.log(req.headers)
 
-    //console.log("DEBUG req " + JSON.stringify(req))
-
-    if(req.get('cookie').indexOf('id_token=OK') !== -1) {
+    hasIdToken(req, res, function(){
         res.sendFile(path.join(__dirname + '/poklad.html'))
-    }else{
+    }, function(){
         res.status(401).send("Unauthorized")
-    }
+    })
+
+})
+
+app.post('/treasure', function(req,res){
+
+    hasIdToken(req, res, function(){
+        res.set('IDKEY', "KlicTotoznosti")
+        res.set('SESSION_KEY', 'ClefSeance')
+        res.send()
+    },function(){
+        res.send()
+    })
+
+
 })
 
 app.get('/logout', function(req, res){
